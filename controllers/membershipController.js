5a4a874fcecb958edf733c73ae9a538417c9c7e6
fsorzzoni <ferminsorzzoni@gmail.com>
2026-03-19 19:48:01 -0300

    membership middlewares

--- controllers/membershipController.js
+++ controllers/membershipController.js
@@ -1,39 +1,18 @@
-import { validationResult, matchedData } from "express-validator";
 import { validateJoinCode } from "../validations/membershipValidations.js";
-import { editUserMembership, isAdminCode, isMemberCode } from "../services/membershipServices.js";
+import { joinCodeHandler } from "../middlewares/memberMiddleware.js";
+import { ensureAuthenticated } from "../middlewares/authMiddleware.js";
 
-function joinClubGet(req, res) {
-    return res.render("enterCodeForm");
-}
+const joinClubGet = [
+    ensureAuthenticated,
+    (req, res) => {
+        return res.render("enterCodeForm");
+    }
+];
 
 const joinClubPost = [
+    ensureAuthenticated,
     validateJoinCode,
-    async (req, res, next) => {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()) {
-            return res.status(400).render("enterCodeForm", {
-                errors: errors.array()
-            });
-        }
-
-        const { code } = matchedData(req);
-
-        const isAdmin = isAdminCode(code);
-        const isMember = isMemberCode(code);
-        if(!isAdmin && !isMember) {
-            return res.status(422).render("enterCodeForm", {
-                errors: [{ msg: "Incorrect code."}]
-            });
-        }
-
-        try {
-            await editUserMembership(id, isAdmin, isMember);
-        } catch(error) {
-            return next(error);
-        }
-
-        return res.redirect("/");
-    }
+    joinCodeHandler
 ];
 
 export { joinClubGet, joinClubPost };
---
+++ middlewares/memberMiddleware.js
@@ -0,0 +1,31 @@
+import { validationResult, matchedData } from "express-validator";
+import { editUserMembership, isAdminCode, isMemberCode } from "../services/membershipServices.js";
+
+async function joinCodeHandler(req, res, next) {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).render("enterCodeForm", {
+            errors: errors.array()
+        });
+    }
+
+    const { code } = matchedData(req);
+
+    const isAdmin = isAdminCode(code);
+    const isMember = isMemberCode(code);
+    if(!isAdmin && !isMember) {
+        return res.status(422).render("enterCodeForm", {
+            errors: [{ msg: "Incorrect code."}]
+        });
+    }
+
+    try {
+        await editUserMembership(id, isAdmin, isMember);
+    } catch(error) {
+        return next(error);
+    }
+
+    return res.redirect("/");
+}
+
+export { joinCodeHandler };
